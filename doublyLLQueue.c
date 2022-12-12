e936ad3198b313afc79cf77da1cdeb5aafc03344
Aravindhan G <80580919+NoobieUs37@users.noreply.github.com>
2022-12-12 09:12:30 +0530

    Update doublyLLQueue.c

--- doublyLLQueue.c
+++ doublyLLQueue.c
@@ -3,13 +3,11 @@
 #include "stdbool.h"
 #define SIZE 5
 
-struct Node {
+typedef struct Node {
 	int data;
 	struct Node *prev;
 	struct Node *next;
-};
-
-typedef struct Node Node;
+} Node;
 
 Node* front = NULL, *rear = NULL;
 int length = 0;
@@ -20,100 +18,71 @@ int dequeue();
 bool isEmpty();
 bool isFull();
 void displayQueue();
-bool searchElement(int);
 
 void main() {
-	printf("\nQueue implementation using linked list\n");
-	int option = 1;
+		printf("\nQueue implementation using doubly linked list\n");
 	while (true) {
-		printf("\n1) Enqueue");
-		printf("\n2) Dequeue");
-		printf("\n3) Check if queue is full or not");
-		printf("\n4) Check if queue is empty or not");
-		printf("\n5) Display queue");
-		printf("\n6) Search an element");
-		int choice;
-		printf("\nEnter choice ");
-		scanf("%d", &choice);
-		switch (choice) {
-			case 0:
-				printf("End of program");
-				return;
+	printf("\n1) Enqueue");
+	printf("\n2) Dequeue");
+	printf("\n3) Check if queue is full or not");
+	printf("\n4) Check if queue is empty or not");
+	printf("\n5) Display queue");
+	int choice;
+	printf("\nEnter choice ");
+	scanf("%d", &choice);
+	switch (choice) {
+		case 0:
+			printf("End of program");
+			return;
 
-			case 1:{
-				if (length == SIZE) {
-					printf("Queue is full\n");
-					break;
-				}
-				int value;
-				printf("Enter value ");
-				scanf("%d", &value);
-				enqueue(value);
+		case 1: {
+			if (length == SIZE) {
+				printf("Queue is full\n");
 				break;
 			}
-
-			case 2: {
-				int dequeuedElement = dequeue();
-				if (!dequeuedElement) {
-					printf("Can't be performed\n");
-					break;
-				}
-				printf("Dequeued Element is %d\n", dequeuedElement);
-				break;
-			}
-
-			case 3: {
-				if (isFull()) {
-					printf("Queue is full\n");
-					break;
-				}
-				printf("Queue is not full\n");
-				break;
-			}
-
-			case 4: {
-				if (isEmpty()) {
-					printf("Queue is empty\n");
-					break;
-				}
-				printf("Queue is not empty\n");
-				break;
-			}
-
-			case 5: {
-				displayQueue();
-				break;
-			}
-
-			case 6: {
-				int element;
-				printf("Enter element ");
-				scanf("%d", element);
-				bool isFound = searchElement(element);
-				if (isFound) {
-					printf("Element exist");
-				} else {
-					printf("Element not existW");
-				}
-			} 
-
-			default:
-				break;
+			int value;
+			printf("Enter value ");
+			scanf("%d", &value);
+			enqueue(value);
+			break;
 		}
-	}
-}
 
-bool searchElement(int element) {
-	if (isEmpty()) {
-		printf("Queue is empty\n");
-		return false;
-	}
-	Node* current = front;
-	while (current != NULL) {
-		if (current->data = element) {
-			return true;
+		case 2: {
+			int dequeuedElement = dequeue();
+			if (!dequeuedElement) {
+				printf("Can't be performed\n");
+				break;
+			}
+			printf("Dequeued Element is %d\n", dequeuedElement);
+			break;
 		}
-		current = current->next;
+
+		case 3: {
+			if (isFull()) {
+				printf("Queue is full\n");
+				break;
+			}
+			printf("Queue is not full\n");
+			break;
+		}
+
+		case 4: {
+			if (isEmpty()) {
+				printf("Queue is empty\n");
+				break;
+			}
+			printf("Queue is not empty\n");
+			break;
+		}
+
+		case 5: {
+			displayQueue();
+			break;
+		}
+
+		default:
+			break;
+	}
 	}
 }
 
@@ -151,6 +120,13 @@ int dequeue() {
 		printf("Queue is empty\n");
 		return 0;
 	}
+    if (front == rear) {
+        Node* temp = rear;
+        front = rear = NULL;
+        int element = temp->data;
+        free(temp);
+        return element;
+    }
 	Node* temp = front;
 	front = temp->next;
 	front->prev = NULL;
@@ -160,7 +136,7 @@ int dequeue() {
 }
 
 void displayQueue() {
-	if (isEmpty()) {
+	if ((isEmpty()) || (front == NULL && rear == NULL)) {
 		printf("Queue is empty\n");
 		return;
 	}
